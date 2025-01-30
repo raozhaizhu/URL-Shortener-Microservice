2a8cbe03f56a622ef4c27f53430be8fabc3c6fc6
raozhaizhu <raozhaizhu@gmail.com>
2025-01-30 16:24:13 +0800

    fix

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const validUrl = require('valid-url'); // 导入 valid-url 库
+const shortid = require('shortid'); // 导入 shortid 库
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json()); // 解析 JSON 请求体
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function (req, res) {
-    res.json({ greeting: 'hello API' });
-});
-
 // 存储URL映射关系
 let urlDatabase = {};
+
 // 创建短链API
 app.post('/api/shorturl', function (req, res) {
     const { url } = req.body;
     // 1.验证URL格式
-    // 1.1如果URL格式不正确，返回错误信息
     if (!validUrl.isWebUri(url)) {
         return res.json({ error: 'invalid url' });
     }
-    // 1.2如果URL格式正确，生成短链
+    // 1.2 如果URL格式正确，生成短链
     const shortUrl = shortid.generate();
     // 2.将短链和URL映射关系存储到数据库中
     urlDatabase[shortUrl] = url;
@@ -44,7 +42,7 @@ app.get('/api/shorturl/:shortUrl', function (req, res) {
     const originalUrl = urlDatabase[shortUrl];
     // 2.如果原始URL不存在，返回错误信息
     if (!originalUrl) {
-        return res.json({ error: 'No such short URL found' });
+        return res.json({ error: 'invalid url' });
     }
     // 3.重定向到原始URL
     res.redirect(originalUrl);
